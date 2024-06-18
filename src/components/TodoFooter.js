deef274137bb6a35b06c1b099795f4afc5971ac9
Oyefolu Toyyib Adeyemi <oyefolutoyyib16@gmail.com>
2024-06-18 09:28:01 +0100

    Completed Todo App

--- src/components/TodoFooter.js
+++ src/components/TodoFooter.js
@@ -1,4 +1,11 @@
-export default function TodoFooter({ todos, setTodos, setFilteredTodos, numTaskLeft }) {
+import { click } from "@testing-library/user-event/dist/click";
+
+export default function TodoFooter({
+  todos,
+  setTodos,
+  setFilteredTodos,
+  numTaskLeft,
+}) {
   const orderNumTaskLeft = numTaskLeft > 1 ? "items" : "item";
 
   function handleAllTask() {
@@ -21,19 +28,32 @@ export default function TodoFooter({ todos, setTodos, setFilteredTodos, numTaskL
     setTodos(todos?.slice().filter((todo) => !todo.completed));
   }
 
+  function handleChangedColor(e) {
+    const clicked = e.target.closest(".change");
+
+    if (!clicked) return;
+
+    const all = document.querySelectorAll(".change");
+    all.forEach((c) => c.classList.remove("change--active"));
+
+    clicked.classList.add("change--active");
+  }
+
   return (
     <footer>
       <p>
         {numTaskLeft} {orderNumTaskLeft} left
       </p>
-      <div className="below">
-        <p className="all" onClick={handleAllTask}>
+      <div className="below" onClick={handleChangedColor}>
+        <p className="all change--active change" onClick={handleAllTask}>
           All
         </p>
-        <p className="active" onClick={handleActiveTask}>
+        <p className="active change" onClick={handleActiveTask}>
           Active
         </p>
-        <p onClick={filterCompletedTask}>Completed</p>
+        <p className="change" onClick={filterCompletedTask}>
+          Completed
+        </p>
       </div>
 
       <p onClick={handleClearCompletedTask}>Clear Completed</p>
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,10 +1,14 @@
 import Items from "./Items";
 
-export default function TodoItems({ filteredTodos, onDeleteTask, onCompletedTask }) {
+export default function TodoItems({
+  filteredTodos,
+  onDeleteTask,
+  onCompletedTask,
+}) {
   return (
     <div className="todo-items">
       <ul>
-        {filteredTodos.map((task) => (
+        {filteredTodos?.map((task) => (
           <Items
             task={task}
             key={task.id}
